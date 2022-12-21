fc79641918371510f9018491955eb3c657f185b0
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2022-12-22 01:24:42 +0530

    first commit

---
+++ 2.js
@@ -0,0 +1,22 @@
+var readlineSync=require('readline-sync');
+
+
+//input
+var userAnswerAge= readlineSync.question("Am I older than 25 ?");
+console.log("You entered "+userAnswerAge);
+
+//processing
+if(userAnswerAge==="yes"){
+    //output
+    console.log("You are right!")
+}
+else{
+    //output
+    console.log("You are wrong!!!")
+
+}
+
+
+
+//output
+console.log(myAge)
