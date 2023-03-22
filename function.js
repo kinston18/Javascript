684a882858278dfef3ff2f74c1a789a7379c3047
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-03-22 22:45:16 +0530

    Building basics

---
+++ function.js
@@ -0,0 +1,26 @@
+function countdown() {
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
+}
+
+// Setting up the the race 🏎 🏎 🏎
+
+countdown()
+
+// GO! 🏁
+// Players are running the race 🏎 💨
+// Race is finished! 🍾
+
+
+// Create a function (you decide the name) that logs out the number 42 to the console
+// Call/invoke the function
+
+function myLogger(){
+    console.log(42)
+}
+myLogger()
+
+
