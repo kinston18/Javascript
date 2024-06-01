2e6b0a538cf549492d40eedacf961d4c55e8cc1b
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2024-06-02 00:27:25 +0530

    JS brush-up3

---
+++ datatypes.js
@@ -0,0 +1,14 @@
+//Reference (Non-Primitive)
+
+//Array, Objects, Functions
+
+const heros = ["Shaktiman","Naagraj", "Doga"]  //Array
+
+let myObj = {
+    name : Prajjwal,
+    age: 22 
+}
+
+const myFunction = function(){
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+}
